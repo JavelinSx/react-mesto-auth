21c1696a9b4a9d914c549ebef88d8917d56d8c44
JavelinSx <zuev499@gmail.com>
2022-11-12 18:16:53 +0300

    test api

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,5 @@
 import Api from "./Api";
-export const api = new Api("https://javelin.nomoredomains.icu", {
+export const api = new Api("https://api.javelin.nomoredomains.icu", {
   "Content-Type": "application/json",
   authorization: "a53b037e-2380-4a96-99e7-f57cd5d08416",
 });
